67c18d4affde949fe7e8ac66a45eefbca3b4d5e9
uves-shaikh <suves58@gmail.com>
2023-03-02 10:37:53 -0800

    nav complete

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -6,8 +6,8 @@ import {BsInstagram} from 'react-icons/bs'
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a href="https://linkedin.com" target="_blank" rel='noreferrer'><BsLinkedin /></a>
-        <a href="https://github.com" target="_blank" rel='noreferrer'><BsGithub /></a>
+        <a href="https://linkedin.com/in/shaikh-uves-3579bb22a" target="_blank" rel='noreferrer'><BsLinkedin /></a>
+        <a href="https://github.com/uves-shaikh" target="_blank" rel='noreferrer'><BsGithub /></a>
         <a href="https://instagram.com" target="_blank" rel='noreferrer'><BsInstagram /></a>
     </div>
   )
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import './Nav.css'
+import {GoHome} from 'react-icons/go'
+import {BiUser} from 'react-icons/bi'
+import {BiBook} from 'react-icons/bi'
+import {RiServiceLine} from 'react-icons/ri'
+import {TiContacts} from 'react-icons/ti'
+import { useState } from 'react'
 
 const Nav = () => {
-  return (
-    <div>Nav</div>
+  const [activeNav, setActiveNav] = useState('#')
+  return ( 
+    <nav>
+      <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><GoHome /></a>
+      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><BiUser /></a>
+      <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BiBook /></a>
+      <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><RiServiceLine /></a>
+      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><TiContacts /></a>
+    </nav>
   )
 }
 
