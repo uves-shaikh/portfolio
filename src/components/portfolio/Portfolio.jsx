3035e4e8a99ad45f279294b513a5e144cdb35e6f
uves-shaikh <suves58@gmail.com>
2023-05-24 22:07:41 +0530

    third project add

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './Portfolio.css'
 import IMG1 from '../../assets/portfolio1.jpg'
 import IMG2 from '../../assets/shoes.png'
+import IMG3 from '../../assets/dentist.png'
 
 const Portfolio = () => {
   return (
@@ -32,18 +33,18 @@ const Portfolio = () => {
           </div>
         </article>
 
-        {/* <article className='portfolio__item'>
+        <article className='portfolio__item'>
           <div className="portfolio__item-image">
-            <img src={IMG1} alt="" />
+            <img src={IMG3} alt="" />
           </div>
-          <h3>My Portfolio</h3>
+          <h3>Appointment For Dentist</h3>
           <div className="portfolio__item-cta">
-            <a href="https://github.com/uves-shaikh/portfolio" className='btn' target="_blank">GitHub</a>
-            <a href="#" className='btn btn-primary' target="_blank">Live Demo</a>
+            <a href="https://github.com/uves-shaikh/SuperDental" className='btn' target="_blank">GitHub</a>
+            <a href="https://superdentall.000webhostapp.com/index.php" className='btn btn-primary' target="_blank">Live Demo</a>
           </div>
         </article>
 
-        <article className='portfolio__item'>
+        {/* <article className='portfolio__item'>
           <div className="portfolio__item-image">
             <img src={IMG1} alt="" />
           </div>
